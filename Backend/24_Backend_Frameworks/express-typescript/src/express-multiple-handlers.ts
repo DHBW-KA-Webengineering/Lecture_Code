104d54f25f11ece7eb1372ee0600f2a7288b4b25
Lukas Panni <lukas.panni@outlook.de>
2026-04-11 16:08:21 +0200

    express typescript examples

---
+++ Backend/24_Backend_Frameworks/express-typescript/src/express-multiple-handlers.ts
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2026 Lukas Panni
+import express from "express";
+
+const app = express();
+app.get(
+  "/",
+  (request, response, next) => {
+    response.locals.responseText = "Antwort von erstem Handler\n";
+    next();
+  },
+  (request, response) => {
+    setTimeout(() => {
+      response.locals.responseText += "Antwort von zweitem Handler";
+      response.send(response.locals.responseText);
+    }, 1000);
+  },
+);
+app.listen(80, () => {
+  console.log("Server gestartet");
+});
