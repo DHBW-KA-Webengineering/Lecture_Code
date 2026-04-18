875aca1a0a78394bc63e9c5c2157ee1776916fa8
Lukas Panni <lukas.panni@outlook.de>
2026-04-18 10:53:28 +0200

    add drizzle sample

---
+++ Backend/25_Datenbanken/drizzle/src/index.ts
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2026 Lukas Panni
+
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import * as schema from "./db/schema";
+
+const main = async () => {
+  const db = drizzle(process.env.DB_FILE_NAME!, { schema });
+  // await db.insert(schema.user).values({ name: "Alice", github_username: "alice" });
+  // await db.insert(schema.user).values({ name: "Bob", github_username: "bob" });
+  // await db.insert(schema.group).values({ name: "Admins" });
+  // await db.insert(schema.group).values({ name: "Users" });
+  // await db.insert(schema.userGroup).values({ user_id: 1, group_id: 1 });
+  // await db.insert(schema.userGroup).values({ user_id: 2, group_id: 2 });
+  // await db.insert(schema.post).values({ title: "Hello", content: "World", author: 1 });
+  // await db.insert(schema.post).values({ title: "Bye", content: "World", author: 2 });
+  // await db.insert(schema.githubAccount).values({ username: "alice" });
+
+  const results = await db.query.user.findFirst({
+    with: {
+      githubAccount: true,
+      posts: true,
+      userGroups: {
+        with: {
+          group: true,
+        },
+      },
+    },
+  });
+  console.log(results);
+};
+
+main();
